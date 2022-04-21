05d4da40b7c0371ef8434a56e7e4424a76cca398
OshinKawle <96054303+OshinKawle@users.noreply.github.com>
2022-04-21 08:23:50 +0530

    Update models.py
    existing

--- models.py
+++ models.py
@@ -6,4 +6,5 @@ class Users(Base):
     id=Column(Integer,primary_key=True,index=True)
     name=Column(String(50))
     co_ordinates=Column(String(100),unique=True)
+    password=Column(String(50))
     
